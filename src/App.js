17909b2693e68fbe5acf52113a4a16e11f4f020c
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2020-11-10 15:52:24 +0530

    created preloader and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
-import Banner from './Banner';
-import Nav from './NavBar';
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./NavBar";
+import Footer from "./Footer";
+import React, { Component } from "react";
+import { CubeGrid } from 'styled-loaders-react'
 
-function App() {
-	return (
-		<div className="app">
-			<Nav />
-			<Banner />
-			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
-			<Row title="Trending" fetchUrl={requests.fetchTrending} />
-			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+export default class App extends Component {
+	constructor(props) {
+		super(props)
+	
+		this.state = {
+			 loading:true
+		}
+	}
+	componentDidMount = () => {
+		setTimeout(() => {
+			this.setState({
+				loading:false
+			})
+		},5000)
+	}
+  render() {
+    return (
+	
+      <div className="app">
+		  {this.state.loading ?
+		<div className="preloader">
+			Developed by: Vaibhav Tiwari
+
 		</div>
-	);
+		: 
+		<di> 
+		<Nav />
+	  <Banner />
+	  <Row
+		title="NETFLIX ORIGINALS"
+		isLargeRow
+		fetchUrl={requests.fetchNetflixOriginals}
+	  />
+	  <Row title="Trending" fetchUrl={requests.fetchTrending} />
+	  <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+	  <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+	  <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+	  <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+	  <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+	  <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+	  <Footer /> 
+	  </di>
+		}
+        
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer-main">
+            <span>
+                created by: Vaibhav Tiwari
+            </span>
+        </div>
+    )
+}
+
+export default Footer
