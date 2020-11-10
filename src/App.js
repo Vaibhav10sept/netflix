61084e0ff9edcdf9653181e3986179a2b22382b0
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2020-11-10 17:09:25 +0530

    enhancing preloader

--- src/App.js
+++ src/App.js
@@ -30,6 +30,16 @@ export default class App extends Component {
 		  {this.state.loading ?
 		<div className="preloader">
 			Developed by: Vaibhav Tiwari
+			<div> <br/>	
+			<CubeGrid size="80px" color="white"/>
+			
+
+			</div>
+			<img
+        className="preloader-img"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
 
 		</div>
 		: 
--- src/NavBar.js
+++ src/NavBar.js
@@ -11,7 +11,7 @@ function Nav() {
       } else handleshow(false);
     });
     return () => {
-      window.removeEventListener("scroll")
+      window.removeEventListener("scroll",null)
     }
   }, []);
 
